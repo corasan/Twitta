641f15581a03c966c1be44ac711ccb6df7af8c76
Henry Paulino <henrypl360@gmail.com>
2020-02-15 00:18:14 -0500

    Initial Commit

---
+++ Twitta/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Twitta
+//
+//  Created by Henry Paulino on 2/15/20.
+//  Copyright © 2020 Henry Paulino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
